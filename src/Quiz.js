68affb46d47afa6bcdcb56ddfe94a419bd501cdf
Catalin Mates <catalinm@Catalin.mates>
2018-07-26 12:30:15 +0300

    Second module

--- src/Quiz.js
+++ src/Quiz.js
@@ -6,14 +6,14 @@ let quizData = require('./quiz_data.json')
 class Quiz extends Component{
     constructor(props){
         super(props);
-        this.state={quiz_position: 1}
+        this.state={quiz_position: 2}
     }
     render(){
         return(
             <div>
                 <QuizQuestion quiz_question={quizData.quiz_question[this.state.quiz_position - 1]}/>
             </div>
-        )
+        );
     }
 }
 
