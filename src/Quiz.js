7e0d0788e641962cb0ef37d4eadf77abb70951df
Catalin Mates <catalinm@Catalin.mates>
2018-07-27 14:19:03 +0300

    4th module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,32 +1,39 @@
-import React, {Component} from 'react'
-import QuizQuestion from './QuizQuestion'
-import QuizEnd from './QuizEnd'
+import React, { Component } from 'react';
+import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
 
-let quizData = require('./quiz_data.json')
+let quizData = require('./quiz_data.json');
 
-class Quiz extends Component{
-    constructor(props){
-        super(props);
-        this.state={quiz_position: 1}
+class Quiz extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {quiz_position: 1};
+  }
+
+  render() {
+    const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length;
+
+    if (isQuizEnd) {
+      return (
+
+        <div>
+
+        <QuizEnd  />
+
+        </div>
+      );
+    } 
+    else {
+      return (
+        <div>
+        <QuizQuestion 
+          quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
+        </div>
+      );
     }
-    render(){
-        const isQuizEnd = true;
-        if(this.state.quiz_position - 1 === quizData.quiz_questions.lenght){
-            return(
-                <div>
-                    <QuizEnd />
-                </div>
-            );
-        }
-        else{
-            return(
-                <div>
-                    <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
-                </div>
-            );
-        }
+  }
 
-    }
 }
 
 export default Quiz;
